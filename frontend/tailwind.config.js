78b8e0045f518e2b60c9fae69b8b07b2ed897af6
Fabian Cruz Garcia <garciacruzfabian23@gmail.com>
2023-06-30 00:08:24 -0600

    feat: adding tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'my-custom-color': '#ffffff',
+      },
+      screens: {
+        xs: '375px',
+        sm: '600px',
+        md: '900px',
+        lg: '1200px',
+        xl: '1536px',
+      },
+    },
+    plugins: [],
+  },
+};
