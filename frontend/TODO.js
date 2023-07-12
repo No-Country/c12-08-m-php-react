4f7eff07cce22868412033c49814bea9b70234d5
Iván <ivanlitt8@gmail.com>
2023-07-12 18:01:48 -0300

    add items in todo list

--- frontend/TODO.js
+++ frontend/TODO.js
@@ -1,1 +1,4 @@
-// TODO: componente Calendar (./components/calendar/Calendar.tsx) agregar feature para marcar dia actual dentro de la semana actual
+// TODO: Componente Calendar (./components/calendar/Calendar.tsx) agregar feature para marcar dia actual dentro de la semana actual
+// TODO: El calendario vista usado en figma es el calendario de Material 3 ( de mobile), se debe reemplazar el diseño usando Material UI (MUI)
+// TODO: Definir en el calendario puequeño, como se marcará el dia actual (siempre fijo) y los otros dias seleccionados
+// TODO: Definir el ruteo que lleva desde el calendario pequeño al grande/principal (Se decidio hacerlo en el String que marca el mes actual). Implemetar tambien al diseño.
