a87e40eebe0be4bd19467e50ed6a80461aef67fe
CJB <carlosjbordon@gmail.com>
2023-07-12 18:46:54 -0300

    navbar

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Layout from '@/components/layout/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Poppins } from 'next/font/google';
@@ -12,7 +13,9 @@ const poppins = Poppins({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${poppins.className} ${poppins.variable} min-h-screen `}>
-      <Component {...pageProps} />;
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
     </main>
   );
 }
