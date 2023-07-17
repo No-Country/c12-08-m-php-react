b693622e1b7aa4501398fe46370aefafd3dc50d0
Bucchiarati <jsalazar005@e.uneg.edu.ve>
2023-07-17 17:55:54 -0500

    Fix:Login/register conditionals
    Added data validations to login and register endpoints

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+
 
 class AuthController extends Controller
 {
@@ -18,25 +20,34 @@ class AuthController extends Controller
      */
     public function register(Request $request)
     {
-        $validatedData = $request->validate([
-                        'name' => 'required|string|max:255',
-                        'surname' => 'required|string|max:255',
-                        'birth' => 'required|date',
-                        'phone' => 'required|numeric|min:10',
-                        'username' => 'required|string|max:255|unique:users',
-                        'email' => 'required|string|email|max:255|unique:users',
-                        'password' => 'required|string|min:8',
-        ]);
+        try{
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'surname' => 'required|string|max:255',
+                'birth' => 'required|date',
+                'phone' => 'required|numeric|min:10',
+                'username' => 'required|string|max:255|unique:users',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8',
+            ]);
 
             $user = User::create([
-                    'name' => $validatedData['name'],
-                    'surname' => $validatedData['surname'],
-                    'birth' => $validatedData['birth'],
-                    'phone' => $validatedData['phone'],
-                    'username' => $validatedData['username'],
-                    'email' => $validatedData['email'],
-                    'password' => Hash::make($validatedData['password']),
+                'name' => $validatedData['name'],
+                'surname' => $validatedData['surname'],
+                'birth' => $validatedData['birth'],
+                'phone' => $validatedData['phone'],
+                'username' => $validatedData['username'],
+                'email' => $validatedData['email'],
+                'password' => Hash::make($validatedData['password']),
             ]);
+        }
+        catch(ValidationException $e){
+            return response()->json([
+                'error' => 'Invalid data',
+                'message' => $e->getMessage(),
+                'errors' => $e->errors()
+            ], 400);
+        }
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
@@ -53,20 +64,45 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-        if (!Auth::attempt($request->only('email', 'password'))) {
-        return response()->json([
-        'message' => 'Invalid login details'
-                ], 401);
+        //valida los datos del request
+        try{
+            $validatedData = $request->validate([
+                'email' => 'required|string|email|max:255',
+                'password' => 'required|string|min:6',
+            ]);
+
+                    //si el correo no tiene un formato valido
+                    if (!filter_var($validatedData['email'], FILTER_VALIDATE_EMAIL)) {
+                        return response()->json([
+                        'error' => 'The email is not valid'
+                        ], 400);
+                    }
+
+            //busca el usuario
+            $user = User::where('email', $validatedData['email'])->first();
+
+            //si no existe el usuario
+            if (!$user || !Hash::check($validatedData['password'], $user->password)) {
+                return response()->json([
+                    'error' => 'The email or password are incorrect'
+                ], 400);
             }
 
-        $user = User::where('email', $request['email'])->firstOrFail();
+            //si existe el usuario
+            $token = $user->createToken('auth_token')->plainTextToken;
 
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json([
+            return response()->json([
                 'access_token' => $token,
                 'token_type' => 'Bearer',
-        ]);
+            ]);
+        }
+        catch(ValidationException $e){
+            return response()->json([
+                'error' => 'Invalid data',
+                'message' => $e->getMessage(),
+                'errors' => $e->errors()
+            ], 400);
+        }
     }
 
     public function me(Request $request)
@@ -77,6 +113,7 @@ class AuthController extends Controller
     public function logout()
     {
         auth()->logout();
+        request()->user()->currentAccessToken()->delete();
 
         return response()->json(['message' => 'Successfully logged out']);
     }
