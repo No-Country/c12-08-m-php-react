70c8942563fde2e07470bab1edfcbe9be996d6ea
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-07-19 19:42:53 -0300

    integrate NewCalendar on CalendarModule

--- frontend/components/CalendarModule/CalendarModule.tsx
+++ frontend/components/CalendarModule/CalendarModule.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import useDatePicker from '@/hooks/useDatePicker';
+import { useState } from 'react';
+// import useDatePicker from '@/hooks/useDatePicker';
 import MedicineList from '../MedicineList/MedicineList';
 // import Calendar from './components/Calendar';
 // import DateDisplay from './components/DateDisplay';
@@ -8,13 +9,18 @@ import NewCalendar from './components/NewCalendar';
 
 const CalendarModule = () => {
   // const { today, selectedDate, pickCurrentDate, selectDate } = useDate();
-  const { selectedDate, selectDate } = useDatePicker();
+  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
 
   return (
     <div className='flex flex-row h-full'>
       {/* <div className='bg-green md:flex-[2] md:rounded-2xl max-md:flex max-md:flex-col max-md:items-center max-md:gap-8'> */}
       <div className='rounded-2xl flex-[2] bg-green'>
-        <NewCalendar selectedDate={selectedDate} selectDate={selectDate} />
+        <NewCalendar
+          initValue={selectedDate}
+          onDateSelect={calendarSelectedDate => {
+            setSelectedDate(calendarSelectedDate);
+          }}
+        />
         {/* <button onClick={pickCurrentDate}>
           <DateDisplay date={today} />
         </button> */}
