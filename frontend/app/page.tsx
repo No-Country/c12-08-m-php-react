7490331a0babaca8196e15b71a7f6d29311f515d
CJB <carlosjbordon@gmail.com>
2023-07-20 06:16:55 -0300

    delete note from index

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,11 @@
 import { LandingNavbar, LandingShowcase } from '@/components';
-import Notes from '@/components/Notes/Notes';
 
 export default function Landing() {
   return (
     <>
       <header>
-        <Notes />
-        {/* <LandingNavbar />
-        <LandingShowcase /> */}
+        <LandingNavbar />
+        <LandingShowcase />
       </header>
     </>
   );
