47d7c5575f61a781ee526ce6ee83fd5fa8c0db07
Iván <ivanlitt8@gmail.com>
2023-07-22 21:36:30 -0300

    feat:GenericContainer added back functionality, GenericContainer now uses 'use client' directive

--- frontend/components/GenericContainer/GenericContainer.tsx
+++ frontend/components/GenericContainer/GenericContainer.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import arrowBack from '/public/svg/arrowBack.svg';
 
 interface Props {
@@ -9,10 +12,17 @@ interface Props {
 }
 
 const GenericContainer = ({ children, color, title, colortitle }: Props) => {
+  const router = useRouter();
+  const handleGoBack = () => {
+    router.back();
+  };
+
   return (
     <div className={`w-full h-full px-2 border rounded-lg shadow-lg bg-${color}`}>
       <header className='flex h-[5%] ml-3 mt-2 top-0'>
-        <Image src={arrowBack} alt='Logo' width={35} height={35} />
+        <div onClick={handleGoBack}>
+          <Image src={arrowBack} alt='Logo' width={35} height={35} />
+        </div>
         <span className={`font-poppins font-semibold text-2xl text-${colortitle}`}>
           {title}
         </span>
