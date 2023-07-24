b0df8114ee836c0e4ce594f3a556793e6fe336fc
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-07-24 01:44:38 -0300

    refact login form

--- frontend/app/(auth)/login/form.tsx
+++ frontend/app/(auth)/login/form.tsx
@@ -1,50 +1,107 @@
-import { Input } from '@/components';
-import email from '@/public/img/email.png';
-import password from '@/public/img/password.png';
+'use client';
+
+import { NewEmailInput, NewPasswordInput, Spinner } from '@/components';
 import { login } from '@/services/auth/auth';
 import { useFormik } from 'formik';
-
 import { useRouter } from 'next/navigation';
+import { ChangeEvent, useState } from 'react';
 import { setCookie } from 'react-use-cookie';
+import * as Yup from 'yup';
+
+interface FormData {
+  email: string;
+  password: string;
+}
 
 const FormLogin = () => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const handleS = async (values: any) => {
-    const { data } = await login(values);
-    console.log(data);
-    setCookie('token', data.access_token, { path: '/', days: 7 });
-    router.push('/home');
+  const onSubmit = async (values: FormData) => {
+    setIsLoading(true);
+    try {
+      const { data } = await login(values);
+      setIsLoading(false);
+      // TODO: once the backend makes the necessary changes to send the token in cookies, this setCookie function should be removed
+      setCookie('token', data.access_token, { path: '/', days: 7 });
+      router.push('/home');
+    } catch (error: any) {
+      setIsLoading(false);
+      if (error.response.status === 400) {
+        setFieldValue('password', '');
+        setStatus('Credenciales incorrectas');
+      }
+      setStatus('Algo salió mal');
+    }
   };
 
-  const { handleSubmit, handleChange } = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-    onSubmit: handleS,
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
+  const {
+    handleSubmit,
+    handleChange,
+    isSubmitting,
+    setFieldValue,
+    status,
+    setStatus,
+    values,
+    errors,
+    touched,
+    handleBlur,
+  } = useFormik({
+    initialValues,
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid mail address').required('Required'),
+      password: Yup.string()
+        .max(20, 'Must be 20 characters or less')
+        .required('Required'),
+    }),
+    onSubmit,
   });
 
+  const isError =
+    Object.keys(errors).length > 0 &&
+    Object.keys(touched).length === Object.keys(initialValues).length;
+
+  const customHandleBlur = (e: FocusEvent) => {
+    setStatus(null);
+    handleBlur(e);
+  };
+
+  const customHandleChange = (e: ChangeEvent) => {
+    setStatus(null);
+    handleChange(e);
+  };
+
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col gap-8 md:gap-4'>
-      <Input
-        src={email}
+    <form onSubmit={handleSubmit} className='flex flex-col items-center gap-8 md:gap-12'>
+      <NewEmailInput
         label='email'
-        type='email'
         name='email'
-        // pattern='^[a-z0-9._+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
-        handleChange={handleChange}
+        value={values.email}
+        handleChange={customHandleChange}
+        onBlur={customHandleBlur}
+        error={errors.email}
       />
-      <Input
-        src={password}
-        label='password'
-        type='password'
+      <NewPasswordInput
+        label='contraseña'
         name='password'
-        // pattern='^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
-        handleChange={handleChange}
+        value={values.password}
+        enableToggler={true}
+        handleChange={customHandleChange}
+        onBlur={customHandleBlur}
+        error={errors.password}
       />
-      <button type='submit' className='btn-secondary shadow'>
-        Iniciar sesión
+      {status ? <div className='text-red-500'>Credenciales incorrectas</div> : null}
+      <button
+        disabled={isSubmitting || isError}
+        className={`${!isError ? 'btn-secondary' : 'btn-error'} w-72 shadow ${
+          isLoading ? 'hover:scale-100' : ''
+        }`}>
+        {isLoading ? <Spinner /> : 'Iniciar Sesión'}
       </button>
     </form>
   );
