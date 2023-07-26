cc91c4043a66f3f92fd998f2a06b4798726d02b9
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-07-26 17:29:41 -0300

    Sidebar fixes

--- frontend/components/Sidebar/Sidebar.tsx
+++ frontend/components/Sidebar/Sidebar.tsx
@@ -1,26 +1,17 @@
 'use client';
 
 import { useWindowsSize } from '@/hooks/useWindowsSize';
-import tailwindConfig from '@/tailwind.config.js';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import resolveConfig from 'tailwindcss/resolveConfig';
 import Panel from './components/Panel';
-
-const getEndpointInt = (endpoint: string) => {
-  const endpointInt = endpoint.replace('px', '');
-  return parseInt(endpointInt);
-};
+import breakpoints from '@/utils/breakpoints';
 
 const Sidebar = () => {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
   const size = useWindowsSize();
 
-  // @ts-ignore: Unreachable code error
-  const breakpoints = resolveConfig(tailwindConfig).theme.screens;
-
   useEffect(() => {
-    if (size.width > getEndpointInt(breakpoints.md)) {
+    if (size.width > breakpoints.md) {
       setSidebarOpen(true);
     } else {
       setSidebarOpen(false);
