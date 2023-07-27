63ee81a7abdb89a1e86e6142b08f3faec9dd32cc
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-07-26 23:53:39 -0300

    update items from BottomNavbar

--- frontend/components/BottomNavbar/BottomNavbar.tsx
+++ frontend/components/BottomNavbar/BottomNavbar.tsx
@@ -1,7 +1,6 @@
+import capsuleIcon from '@/public/svg/capsule.svg';
 import homeIcon from '@/public/svg/nb-home.svg';
 import notesIcon from '@/public/svg/nb-notes.svg';
-import plansIcon from '@/public/svg/nb-plans.svg';
-import subsIcon from '@/public/svg/nb-subs.svg';
 import BottomNavbarItem from './components/BottomNavbarItem';
 
 const items = [
@@ -11,26 +10,21 @@ const items = [
     title: 'Inicio',
   },
   {
-    to: '/subs',
-    iconSrc: subsIcon,
-    title: 'Subscripciones',
+    to: '/home/meds',
+    iconSrc: capsuleIcon,
+    title: 'Mis medicinas',
   },
   {
     to: '/notes',
     iconSrc: notesIcon,
     title: 'Notas',
   },
-  {
-    to: '/home/meds',
-    iconSrc: plansIcon,
-    title: 'Medicinas',
-  },
 ];
 
 const BottomNavbar = () => {
   return (
     <footer className='z-30 w-full max-md:h-[8%] bg-white border-t-2 border-lightGreen  md:hidden'>
-      <div className='grid h-full max-w-lg grid-cols-4 mx-auto font-medium'>
+      <div className='flex justify-evenly h-full max-w-lg mx-auto font-medium'>
         {items.map(item => (
           <BottomNavbarItem key={item.title} item={item} />
         ))}
