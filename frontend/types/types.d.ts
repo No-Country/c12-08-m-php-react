ddcd26d6de677937fda8afb51f174ec5a28ff3ef
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-07-27 23:18:27 -0300

    add types.d.ts file

---
+++ frontend/types/types.d.ts
@@ -0,0 +1,28 @@
+export interface NoteData {
+  id: number;
+  title: string;
+  description: string;
+}
+
+export interface User {
+  name: string;
+  surname: string;
+  birth: string;
+  email: string;
+  username: string;
+  photo_url: string;
+  phone: string;
+  gender: 'Femenino' | 'Masculino' | 'Prefiero no decirlo';
+  id: number;
+  created_at: Date;
+}
+
+interface Item {
+  name: string;
+  indications: string;
+  quantity: number;
+  is_single_dose: boolean;
+  init_date: Date;
+  time: number;
+  category_id: number;
+}
