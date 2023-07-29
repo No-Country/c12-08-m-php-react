de1e0fc858d17523554047d7d34e34188a9618ca
Bucchiarati <jsalazar005@e.uneg.edu.ve>
2023-07-28 21:27:52 -0500

    Fix: CSRF Middleware
    Fix Cross-origin try 2

--- backend/app/Http/Middleware/VerifyCsrfToken.php
+++ backend/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,8 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/api/login',
-        '/api/register',
+        'api/*'
     ];
 
     //Verifica si el token es valido
