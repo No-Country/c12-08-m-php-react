e0d685a282a9b688396fb512a44f1e7124bc62ad
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-08-01 17:21:01 -0300

    enhace middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,20 +1,30 @@
 import { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+import verifyToken from '@/services/auth/verify-token';
 
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const isPathPublic = path === '/login' || path === '/register';
   const token = request.cookies.get('jwt_token')?.value || '';
+  const isAuth = await verifyToken(token);
 
-  if (isPathPublic && token) {
+  if (isPathPublic && isAuth) {
     return NextResponse.redirect(new URL('/home', request.url));
   }
 
-  if (!isPathPublic && !token) {
-    return NextResponse.redirect(new URL('/login', request.url));
+  if (!isPathPublic && !isAuth) {
+    const response = NextResponse.redirect(new URL('/login', request.url));
+    response.cookies.set({ name: 'jwt_token', value: '', path: '/', expires: 0 });
+    return response;
   }
 
-  NextResponse.next();
+  if (!isAuth) {
+    const response = NextResponse.next();
+    response.cookies.set({ name: 'jwt_token', value: '', path: '/', expires: 0 });
+    return response;
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
